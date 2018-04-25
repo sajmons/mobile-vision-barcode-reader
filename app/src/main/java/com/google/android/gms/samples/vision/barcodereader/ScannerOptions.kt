34911c6a32262eed333d44f05dc9f6a6c81d0f3d
Simon Šander <simon.sander@vizija.si>
2018-04-25 14:49:55 +0200

    - targeting latest google libraries

--- app/src/main/java/com/google/android/gms/samples/vision/barcodereader/ScannerOptions.kt
+++ app/src/main/java/com/google/android/gms/samples/vision/barcodereader/ScannerOptions.kt
@@ -2,7 +2,12 @@ package com.google.android.gms.samples.vision.barcodereader
 
 import android.hardware.Camera
 
-class ScannerOptions(focusMode: String, flashMode: String, barcodeFormats: Array<Int>,
-                     cameraFacing: Int = Camera.CameraInfo.CAMERA_FACING_BACK, fps: Float, previewWidth: Int = 1600, previewHeight: Int = 1024) {
+class ScannerOptions(focusMode: String,
+                     flashMode: String,
+                     barcodeFormats: Array<Int>,
+                     cameraFacing: Int = Camera.CameraInfo.CAMERA_FACING_BACK,
+                     fps: Float,
+                     previewWidth: Int = 1600,
+                     previewHeight: Int = 1024) {
 
 }
