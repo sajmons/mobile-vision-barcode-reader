feede8363a8c3a2c740b996e3dfd7006488a5a38
Simon Šander <simon.sander@vizija.si>
2018-04-25 14:32:53 +0200

    - initial commit

---
+++ app/src/main/java/com/google/android/gms/samples/vision/barcodereader/ScannerOptions.kt
@@ -0,0 +1,8 @@
+package com.google.android.gms.samples.vision.barcodereader
+
+import android.hardware.Camera
+
+class ScannerOptions(focusMode: String, flashMode: String, barcodeFormats: Array<Int>,
+                     cameraFacing: Int = Camera.CameraInfo.CAMERA_FACING_BACK, fps: Float, previewWidth: Int = 1600, previewHeight: Int = 1024) {
+
+}
